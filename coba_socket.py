fe8ccca5656a473bfae48d41ec5f592b63b0ed61
M Daud <56988523+muhammaddaud23@users.noreply.github.com>
2021-10-19 10:00:32 +0700

    Initial Commit

---
+++ coba_socket.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import socket
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+HOST = '127.0.0.1'
+PORT = 50080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_inet untuk IPv4, sedangkan SOCK_stream untuk tcp
+s.connect((HOST, PORT))
+
+start_time = time.time()
+result = np.array([])
+
+try:
+	while True:
+		msg = s.recv(512)
+		tem = np.frombuffer(msg,dtype=np.float32)
+		result = np.concatenate([result,tem])
+		#print(type(tem))
+except KeyboardInterrupt:
+	pass
+
+print(len(tem))
+duration = start_time - time.time()
+
+y = np.array(result)
+x = np.linspace(0,duration,len(result))
+plt.scatter(x,y,color='black')
+plt.xlabel('Time')
+plt.ylabel('Strength')
+plt.show()
